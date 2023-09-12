b4e9943296d1e6bada99c8cdcc686e611a6ffa99
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2023-09-12 11:37:47 +0300

    new http-common

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,9 +1,24 @@
 import axios from 'axios'
 import config from './data/config'
+import { userToken } from './services/authUser.service'
 
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 export const apiClient = axios.create({
   baseURL: config.API_URL,
-  headers: {
-    'Content-type': 'application/json'
-  }
 })
+
+apiClient.defaults.headers.common['Content-Type'] = 'application/json'
+apiClient.defaults.headers.common['Content-Type'] = 'multipart/form-data'
+
+apiClient.interceptors.request.use (
+  function (config) {
+    const token = userToken()
+    console.log('HTTP token: ', token)
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+  },
+  function (error) {
+    console.log('HTTP error: ', error)
+    return Promise.reject(error)
+  }
+)
